e7867c21d1cf82d78369f5a7ba197baeeb7823bb
CandycanGrunge <tabina1201@gmail.com>
2023-12-30 13:46:38 +0800

    Sign in page done [No button action yet]

--- app/src/main/java/com/example/myapptest/MainActivity.java
+++ app/src/main/java/com/example/myapptest/MainActivity.java
@@ -6,14 +6,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.auth.User;
 
 import java.util.HashMap;
 import java.util.Map;
