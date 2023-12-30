9276344512ed9a6e34f0a37d3d0b42174be7cdfb
ravenCrown0627 <60862845+ravenCrown0627@users.noreply.github.com>
2023-12-30 17:07:22 +0800

    Done added Firebase Auth for login system

--- app/src/main/java/com/putra/management/MainActivity.java
+++ app/src/main/java/com/putra/management/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.putra.management.HomePage;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private MaterialButton submit_btn;
 
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,39 +44,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // To write data to the database, call the set() method, passing in a Map of key-value pairs.
-    public void saveInfo(View v) {
-        String username = editTextUsername.getText().toString();
+    public void loginAuth(View v) {
+        String email = editTextUsername.getText().toString();
         // For email registration, is the email has ******@student.upm.edu.my ==> No need request mail address from user, just matric number
         String password = editTextPassword.getText().toString();
 
-        Map<String, Object> user = new HashMap<>();
-        user.put(KEY_USERNAME, username);
-        user.put(KEY_PASSWORD, password);
+        // Check if email and password is empty
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // Check if email is valid
+        else if (!email.contains("@student.upm.edu.my")) {
+            Toast.makeText(MainActivity.this, "Please insert the valid student email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // Check if password is valid
+        else if (password.length() < 6) {
+            Toast.makeText(MainActivity.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else {
+            // Check if email and password is valid
+            FirebaseUser currentUser = mAuth.getCurrentUser();
 
-        // Add user to database, handle both success and failure
-        db.collection("users").document(username).set(user)
-            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                @Override
-                public void onSuccess(Void aVoid) {
-                    Toast.makeText(MainActivity.this, "User added to database", Toast.LENGTH_SHORT).show();
-                    Log.d("SUCCESS", "User added to database");
-
-                    openHome(); // if Successful, will go to 'next page' --- Home page
+            mAuth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<com.google.firebase.auth.AuthResult>() {
+                    @Override
+                    public void onSuccess(com.google.firebase.auth.AuthResult authResult) {
+                        Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        openHome();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    }
                 }
-            })
-            .addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(Exception e) {
-                    Toast.makeText(MainActivity.this, "Error adding user to database", Toast.LENGTH_SHORT).show();
-                    Log.d("FAIL", "Error adding user to database");
-                }
-            }
-        );
-
-
-
+            );
+        }
     }
-
 
 // The class for the home page options (non-admin)
 public void openHome() {
