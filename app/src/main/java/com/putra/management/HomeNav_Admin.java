9bdee37ac071530054919cffb55b7ad4b9886c49
CandycanGrunge <tabina1201@gmail.com>
2024-01-01 17:52:30 +0800

    Sign Up Dropdown Completed

---
+++ app/src/main/java/com/putra/management/HomeNav_Admin.java
@@ -0,0 +1,14 @@
+package com.putra.management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeNav_Admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_nav_admin);
+    }
+}
